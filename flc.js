cce577d8e8c780428f6cd6c582f7ae3b2a5cd348
Nantha Kumar.T <155755964+nanthakumar032001@users.noreply.github.com>
2024-02-16 16:52:24 +0530

    Add files via upload

---
+++ flc.js
@@ -0,0 +1,13 @@
+var slidenavbar=document.querySelector(".slidenav")
+
+function slidemenu()
+{
+    slidenavbar.style.display="block"
+
+}
+function slideclose()
+{
+    slidenavbar.style.display="block"
+    
+}
+
